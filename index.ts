5a3b32b91f3ecf5a8667a0120635aa9bd9a46f4a
Tony Keiser <keisertony@gmail.com>
2025-04-04 22:47:40 -0600

    Agent

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
 import 'dotenv/config'
-import { runLLM } from './src/llm'
-import { addMessages, getMessages } from './src/memory'
+import { runAgent } from './src/agent'
+import { z } from 'zod'
+
 const userMessage = process.argv[2]
 
 if (!userMessage) {
   console.error('Please provide a message')
   process.exit(1)
 }
+const weatherTool = {
+  name: 'get_weather',
+  parameters: z.object({}),
+}
 
-await addMessages([{ role: 'user', content: userMessage }])
-const messages = await getMessages()
-
-const response = await runLLM({ messages })
-
-await addMessages([{ role: 'assistant', content: response }])
+const response = await runAgent({ userMessage, tools: [weatherTool] })
 
 console.log(response)
